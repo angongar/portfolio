d985d535f295c6f48033b68b30bfc7c847a9aefb
angongar <antoniomanuel2094@gmail.com>
2025-08-10 18:22:43 +0200

    Configuracion de la pantalla CV

--- src/main/java/com/tonigdev/portfolio/controllers/ExperienceController.java
+++ src/main/java/com/tonigdev/portfolio/controllers/ExperienceController.java
@@ -7,9 +7,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tonigdev.portfolio.model.entities.Education;
+import com.tonigdev.portfolio.model.entities.Experience;
 import com.tonigdev.portfolio.model.entities.ExternalLink;
 import com.tonigdev.portfolio.model.entities.Profile;
 import com.tonigdev.portfolio.model.entities.User;
+import com.tonigdev.portfolio.services.education.EducationService;
+import com.tonigdev.portfolio.services.experience.ExperienceService;
 import com.tonigdev.portfolio.services.externallink.ExternalLinkService;
 import com.tonigdev.portfolio.services.profile.ProfileService;
 import com.tonigdev.portfolio.services.user.UserService;
@@ -24,17 +28,29 @@ public class ExperienceController {
 	private final UserService userService;
 	private final ProfileService profileService;
 	private final ExternalLinkService externalLinkService;
+	private final ExperienceService experienceService;
+	private final EducationService educationService;
 
 	@GetMapping
 	public String experience(Model model) {
 		User user = userService.getUser().get();
 		Profile profile = profileService.findByUserId(user.getId()).get();
 		List<ExternalLink> links = externalLinkService.findByUserId(user.getId());
+		List<Education> educations = educationService.findByUserId(user.getId());
+		List<Experience> experiences = experienceService.findByUserId(user.getId());
+		
+		System.out.println("Formacion");
+		educations.forEach(e -> System.out.println(e.getTitle() + " - Desde " + e.getDateStart() + " hasta " + e.getDateEnd()));
+		System.out.println("Experiencia laboral");
+		experiences.forEach(e -> System.out.println(e.getCompany() + " - Desde " + e.getDateStart() + " hasta " + e.getDateEnd()));
+		
 		
 		model.addAttribute("currentPage", "experience");
 		model.addAttribute("user", user);
 		model.addAttribute("profile", profile);
 		model.addAttribute("links", links);
+		model.addAttribute("educations", educations);
+		model.addAttribute("experiences", experiences);
 		return "experience";
 	}
 	
